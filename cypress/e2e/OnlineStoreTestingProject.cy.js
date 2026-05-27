dd8ae96018569774015bf0907ddedd62cfcec6b4
Tabixx <panalien10@gmail.com>
2026-05-27 16:38:32 +0200

    Added Basic Cart Tests

--- cypress/e2e/OnlineStoreTestingProject.cy.js
+++ cypress/e2e/OnlineStoreTestingProject.cy.js
@@ -1,12 +1,14 @@
 import HomePage from "../page-objects/homePage";
 import AccountPage from "../page-objects/myAccountPage";
+import CartPage from "../page-objects/cartPage";
 import { faker } from '@faker-js/faker'
 
 
 
 describe('OnlineStoreTestingProject', () => {
-    const homePage = new HomePage()
-    const accountPage = new AccountPage()
+    const homePage = new HomePage();
+    const accountPage = new AccountPage();
+    const cartPage = new CartPage();
 
     before(function () {
         cy.fixture('users.json').as('userData')
@@ -26,5 +28,13 @@ describe('OnlineStoreTestingProject', () => {
         accountPage.fillPasswordField(faker.internet.password())
         accountPage.clickLoginButton()
         accountPage.checkVisibilityOfErrorAfterWrongLogin()
+    })
+    it('Test Adding Product to Cart and then Deleting it', function () {
+        homePage.visitPage()
+        homePage.addProductToCart()
+        homePage.clickGoToCartFromProductButton()
+        cartPage.checkThatAddedProductIsInCart()
+        cartPage.removeItemFromCart()
+        cartPage.checkThatCartIsEmpty()
     })
 })
